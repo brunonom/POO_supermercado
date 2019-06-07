a19f88de1639f4180361e0e2b7af649b8360e72d
brunonom <38799938+brunonom@users.noreply.github.com>
2019-06-07 16:03:39 -0300

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,4 +1,8 @@
+javac Setor.java
+javac Pessoa.java
+javac Cliente.java
 javac Funcionario.java
-javac Produto.java
 javac AtendenteCaixa.java
+javac Gerente.java
+javac Produto.java
 javac Item.java
